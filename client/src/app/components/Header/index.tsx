69f77a76fff9c429f333a8b105de79e9ff755ebb
liu till real <duccdht123@gmail.com>
2023-12-24 18:58:32 +0700

    sleep

--- client/src/app/components/Header/index.tsx
+++ client/src/app/components/Header/index.tsx
@@ -1,100 +1,72 @@
-"use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import Avatar from "../Avatar";
 
 export default function Header() {
   return (
-    <Navbar fixed="top" expand="lg">
-      <Container>
-        <Navbar.Brand
-          href="/"
-          as={Link}
-          className="position-relative"
-          style={{ width: 36, height: 36 }}
-        >
-          <Image src="/logo.svg" alt="logo" fill />{" "}
-        </Navbar.Brand>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        {/* Logo */}
+        <Link href="/" className="navbar-brand">
+          <Image src="/logo.svg" alt="Logo" width={36} height={36} />
+        </Link>
 
-        <Navbar.Toggle
-          bsPrefix="navbar-toggler ms-auto btn-light icon-md p-0"
+        {/* Menu */}
+        <button
+          type="button"
+          title="Menu"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
           aria-controls="navbarCollapse"
-          children={
-            <span className="navbar-toggler-animation">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-          }
-        />
-        <Navbar.Collapse id="navbarCollapse">
-          <Nav as="ul" className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <Nav.Item as="li" className="nav-item">
-              <Nav.Link as={Link} href="/" className="nav-link">
+          aria-expanded="false"
+          className="navbar-toggler ms-auto icon-md btn btn-light p-0 collapsed"
+        >
+          <span className="navbar-toggler-animation">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </button>
+
+        {/* Collapse */}
+        <div className="navbar-collapse collapse" id="navbarCollapse">
+          {/* Nav Search */}
+          <div className="nav mt-3 mt-lg-0 flex-nowrap align-items-center px-4 px-lg-0">
+            <form className="position-relative w-100">
+              <input
+                type="search"
+                className="form-control ps-5 bg-light"
+                placeholder="Search..."
+                aria-label="Search"
+              />
+              <button
+                type="submit"
+                className="position-absolute top-50 start-0 btn bg-transparent px-2 py-0 translate-middle-y"
+              >
+                <i className="bi bi-search fs-5"></i>
+              </button>
+            </form>
+          </div>
+
+          <ul className="navbar-nav navbar-nav-scroll ms-auto">
+            <li className="nav-item">
+              <Link href="/" className="nav-link">
                 Home
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-
-        <Nav as="ul" className="flex-row align-items-center ms-sm-3">
-          <Nav.Item as="li" className="ms-2">
-            <Nav.Link
-              as={Link}
-              title="Chats"
-              href="/chats"
-              className="bg-light icon-md btn btn-light p-0"
-            >
-              <i className="bi bi-chat-left-text-fill fs-6"></i>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li" className="ms-2">
-            <Nav.Link
-              as={Link}
-              title="Settings"
-              href="/settings"
-              className="bg-light icon-md btn btn-light p-0"
-            >
-              <i className="bi bi-gear-fill fs-6"></i>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li" className="ms-2">
-            <Nav.Link
-              type="button"
-              as="button"
-              title="Notifications"
-              className="bg-light icon-md btn btn-light p-0 position-relative"
-            >
-              <span
-                className="position-absolute top-0 p-1 bg-danger rounded-circle"
-                style={{ right: "-3px" }}
-              ></span>
-              <i className="bi bi-bell-fill fs-6"></i>
-            </Nav.Link>
-          </Nav.Item>
-
-          <Nav.Item as="li" className="ms-2 dropdown">
-            <Nav.Link
-              type="button"
-              title="Profile"
-              as="button"
-              data-bs-toggle="dropdown"
-              className="bg-light icon-md btn btn-light p-0 position-relative"
-            >
-              <Avatar src="/07.jpg" />
-            </Nav.Link>
-
-            <ul className="dropdown-menu">
-              <li>
-                <Link href="/" className="dropdown-item">
-                  Home
-                </Link>
-              </li>
-            </ul>
-          </Nav.Item>
-        </Nav>
-      </Container>
-    </Navbar>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/friends" className="nav-link">
+                Friends
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/groups" className="nav-link">
+                Groups
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
